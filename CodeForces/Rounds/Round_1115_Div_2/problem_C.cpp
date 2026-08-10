6c74db4e992306084dcdc9b1e88a29410473912b
LCS-Dev <xtremexspc@gmail.com>
2026-08-11 00:57:59 +0200

    feat(R1115D2): Add solution for problem C.

--- CodeForces/Rounds/Round_1115_Div_2/problem_C.cpp
+++ CodeForces/Rounds/Round_1115_Div_2/problem_C.cpp
@@ -7,6 +7,7 @@
 
 #define NEED_MACROS
 #define NEED_TIMER
+#define NEED_TYPES
 #define CP_IO_PROFILE_SIMPLE
 
 #include "templates/Base.hpp"
@@ -15,7 +16,38 @@
 /* Main Solver Function */
 
 void solve() {
-  // Optimized solution here
+  INT(n, m);
+  VecI64 v(n);
+  VecI32 a(n * m);
+  IN(v, a);
+
+  MultiSet<I32> top;
+  I32 ans = m;
+
+  FOR_R(i, n) {
+    FOR(j, m) {
+      I32 x = a[i * m + j];
+      if (isz(top) < m) {
+        top.insert(x);
+      } else if (x > *top.begin()) {
+        top.erase(top.begin());
+        top.insert(x);
+      }
+    }
+
+    I64 sum = 0;
+    I32 cnt = 0;
+    for (auto it = top.rbegin(); it != top.rend() && cnt < ans; ++it) {
+      sum += *it;
+      ++cnt;
+      if (sum >= v[i]) {
+        ans = cnt;
+        break;
+      }
+    }
+  }
+
+  OUT(ans);
 }
 
 //===----------------------------------------------------------------------===//
